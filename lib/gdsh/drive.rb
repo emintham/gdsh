fa483005d40c64dc9a9bae59cd38e821347bc7d7
Emin Tham <emintham@gmail.com>
2014-06-11 12:41:55 -0400

    refactored drive.rb

--- lib/gdsh/drive.rb
+++ lib/gdsh/drive.rb
@@ -17,17 +17,33 @@ class DriveService
     # default to per-file permissions
     @oauth_scope = 'https://www.googleapis.com/auth/drive.file'
     @redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+    @filename = filename
 
     if filename && File.exist?(filename)
-      File.open(filename, 'r') do |f|
-        buffer = f.read
-        credentials = JSON.parse(buffer)
-        @client_id = credentials['installed']['client_id']
-        @client_secret = credentials['installed']['client_secret']
-      end
+      credentials_from_file
     else
-      get_credentials
+      credentials_from_stdin
     end
+  end
+
+  def credentials_from_file
+    File.open(@filename, 'r') do |f|
+      buffer = f.read
+      credentials = JSON.parse(buffer)
+      @client_id = credentials['installed']['client_id']
+      @client_secret = credentials['installed']['client_secret']
+    end
+  end
+
+  def init_client
+    # Create a new API client & load the Google Drive API
+    @client = Google::APIClient.new
+
+    # Request authorization
+    @client.authorization.client_id = @client_id
+    @client.authorization.client_secret = @client_secret
+    @client.authorization.scope = @oauth_scope
+    @client.authorization.redirect_uri = @redirect_uri
   end
 
   ##
@@ -36,30 +52,21 @@ class DriveService
   # command line to authorize access.
   #
   def authorize
-    # Create a new API client & load the Google Drive API
-    client = Google::APIClient.new
+    init_client
 
-    # Request authorization
-    client.authorization.client_id = @client_id
-    client.authorization.client_secret = @client_secret
-    client.authorization.scope = @oauth_scope
-    client.authorization.redirect_uri = @redirect_uri
-
-    uri = client.authorization.authorization_uri
+    uri = @client.authorization.authorization_uri
     Launchy.open(uri)
 
     # Exchange authorization code for access token
     print 'Enter authorization code: '
-    client.authorization.code = $stdin.gets.chomp
-    client.authorization.fetch_access_token!
-
-    @client = client
+    @client.authorization.code = $stdin.gets.chomp
+    @client.authorization.fetch_access_token!
   end
 
   ##
   # Get credentials from shell if no credentials file was specified.
   #
-  def get_credentials
+  def credentials_from_stdin
     # get preset if exists
     @client_id ||= ''
     @client_secret ||= ''
